403c2efafd6ab6505e58f6df93aef532bc9f9814
Asandu178 <dragos310131@gmail.com>
2025-10-26 21:09:45 +0200

    Finished leet 62

---
+++ leet/62.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int uniquePath(int m, int n) {
+    int dp[100][100] = {0};
+    int dp[0][0] = 1;
+    for (int i = 0 ; i < m ; i++) {
+        for (int j = 0 ; j < n ; j++) {
+            if (i - 1 >= 0)
+                dp[i][j] += dp[i - 1][j];
+            if (j - 1 >= 0)
+                dp[i][j] += dp[i][j - 1];
+        }
+    }
+    return dp[m - 1][n - 1];
+}
